e2f8acb231a3327c0582501061c1020d02a44add
krishna121121 <krishnamadhavjha121@gmail.com>
2026-06-28 01:13:18 +0530

    a small change

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,8 @@
-require("dotenv").config()
-const app=require('./src/app');
-const connectToDb=require('./src/config/database');
+require("dotenv").config();
 
-
-
-
+const app = require("./src/app");
+const connectToDb = require("./src/config/database");
 
 connectToDb();
 
-
-
-app.listen(3000,()=>{
-    console.log("server is running at port 3000");
-})
+module.exports = app;
