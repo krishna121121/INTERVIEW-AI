0193fd2a8fb8cd05d605f05fbb60fc3d92c58d73
krishna121121 <krishnamadhavjha121@gmail.com>
2026-07-04 19:21:26 +0530

    small change in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,13 @@ connectToDb();
 
 const PORT = process.env.PORT || 3000;
 
+
+app.get("*",(req,res)=>{
+    res.status(200).json({
+        message:"successfull"
+    })
+})
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
